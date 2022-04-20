66a329123699be4dca28e79e068f50de86951509
sbell9724 <somibell@ucsc.edu>
2022-04-19 18:21:36 -0700

    Comments Made

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
+// Solomon Bell, Rocket Patrol Mod, 4/19/2022, ~5 Hours
+// 60 points for full aesthetic overhaul
+// 5 points for high score tracking
+// 5 points for 'Fire' UI text
+// 5 points for copyright free soundtrack (The song 'Badass' at the link https://www.bensound.com/royalty-free-music/funky-groove)
+// 5 points for allowing the player to control the ball after shot
+// 20 points for creating a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points
+
+
 let config = {
     width: 640,
     height: 480,
